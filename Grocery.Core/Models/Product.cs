c1e43ccc2c35cc80c614fbfa2df2fc0f9b36fedd
Ruben <s1210228@student.windesheim.nl>
2025-10-06 16:57:50 +0200

    Branch update naar sprint 5 startpunt - THT datum toegevoegd

--- Grocery.Core/Models/Product.cs
+++ Grocery.Core/Models/Product.cs
@@ -5,10 +5,18 @@ namespace Grocery.Core.Models
     public partial class Product : Model
     {
         [ObservableProperty]
-        private int stock;
-        public Product(int id, string name, int stock) : base(id, name)
+        public int stock;
+        public DateOnly ShelfLife { get; set; }
+        public Product(int id, string name, int stock) : this(id, name, stock, default) { }
+
+        public Product(int id, string name, int stock, DateOnly shelfLife) : base(id, name)
         {
             Stock = stock;
+            ShelfLife = shelfLife;
+        }
+        public override string? ToString()
+        {
+            return $"{Name} - {Stock} op voorraad";
         }
     }
 }
