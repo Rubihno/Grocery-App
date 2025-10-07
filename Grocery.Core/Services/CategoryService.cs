e3659d8f0d98ba59b0569ce8f5478492dcd7f1fd
Rubihno <rubenhakvoort@gmail.com>
2025-10-07 14:37:22 +0200

    Basis methods Category en ProductCategory services toegevoegd

---
+++ Grocery.Core/Services/CategoryService.cs
@@ -0,0 +1,34 @@
+﻿using Grocery.Core.Interfaces.Repositories;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepostitory;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepostitory = categoryRepository;
+        }
+
+        public Category? Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Grocery.Core/Services/ProductCategoryService.cs
@@ -0,0 +1,34 @@
+﻿using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Services
+{
+    public class ProductCategoryService : IProductCategoryService
+    {
+        public Category? Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+        public void Remove(Product product)
+        {
+            throw new NotImplementedException();
+        }
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
