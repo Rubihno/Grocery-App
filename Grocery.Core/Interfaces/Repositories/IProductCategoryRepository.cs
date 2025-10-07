b5f5357dbbf7acbbe365e74a12c3cf40d1a2eea8
Rubihno <rubenhakvoort@gmail.com>
2025-10-07 14:29:05 +0200

    Interfaces voor Category en ProductCategory services en repositories toegevoegd

---
+++ Grocery.Core/Interfaces/Repositories/IProductCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Grocery.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Interfaces.Repositories
+{
+    public interface IProductCategoryRepository
+    {
+        public Category? Get(int id);
+        public List<Category> GetAll();
+        public void Add(Product product);
+        public void Remove(Product product);
+        public void Update();
+    }
+}
---
+++ Grocery.Core/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Grocery.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Interfaces.Services
+{
+    public interface ICategoryService
+    {
+        public Category? Get(int id);
+        public List<Category> GetAll();
+        public void Add(Category category);
+    }
+}
