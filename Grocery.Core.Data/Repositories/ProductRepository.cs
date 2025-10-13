406e2bbdb2918d7a35e6801e6ba2e80aea14e464
Ruben <s1210228@student.windesheim.nl>
2025-10-13 16:32:14 +0200

    Repo update naar start fase sprint 6 - Database connection en code toegevoegd - GroceryListReporitory toegevoegd aan database

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -9,10 +9,10 @@ namespace Grocery.Core.Data.Repositories
         public ProductRepository()
         {
             products = [
-                new Product(1, "Melk", 300, new DateOnly(2025, 9, 25), 1.45m),
-                new Product(2, "Kaas", 100, new DateOnly(2025, 9, 30), 4.50m),
-                new Product(3, "Brood", 400, new DateOnly(2025, 9, 12), 1.99m),
-                new Product(4, "Cornflakes", 0, new DateOnly(2025, 12, 31), 2.99m)];
+                new Product(1, "Melk", 300, 1.45m, new DateOnly(2025, 9, 25)),
+                new Product(2, "Kaas", 100, 4.50m, new DateOnly(2025, 9, 30)),
+                new Product(3, "Brood", 400, 1.99m, new DateOnly(2025, 9, 12)),
+                new Product(4, "Cornflakes", 0, 2.99m, new DateOnly(2025, 12, 31))];
         }
         public List<Product> GetAll()
         {
